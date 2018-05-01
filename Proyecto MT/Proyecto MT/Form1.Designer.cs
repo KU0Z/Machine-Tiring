0510ad57af46b8cc63ab7d70d69c646f4392d936
Kevin Ubedo <koubedo@correo.url.edu.gt>
2018-05-01 16:16:58 -0600

    MAquina 1 terminada

--- Proyecto MT/Proyecto MT/Form1.Designer.cs
+++ Proyecto MT/Proyecto MT/Form1.Designer.cs
@@ -29,26 +29,43 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
             this.DgvCinta = new System.Windows.Forms.DataGridView();
             this.btnIniciar = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.tbEntrada = new System.Windows.Forms.TextBox();
             this.btnCargar = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.btnDetener = new System.Windows.Forms.Button();
             this.tbVelocidad = new System.Windows.Forms.TrackBar();
             this.dgvTranciciones = new System.Windows.Forms.DataGridView();
+            this.btnnext = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbpasos = new System.Windows.Forms.Label();
+            this.lbEstado = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DgvCinta)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tbVelocidad)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvTranciciones)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // DgvCinta
             // 
             this.DgvCinta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.DgvCinta.ColumnHeadersVisible = false;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.Green;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgvCinta.DefaultCellStyle = dataGridViewCellStyle7;
+            this.DgvCinta.Enabled = false;
             this.DgvCinta.Location = new System.Drawing.Point(43, 52);
             this.DgvCinta.Name = "DgvCinta";
             this.DgvCinta.ReadOnly = true;
@@ -58,11 +75,11 @@
             // 
             // btnIniciar
             // 
-            this.btnIniciar.Location = new System.Drawing.Point(43, 180);
+            this.btnIniciar.Location = new System.Drawing.Point(3, 3);
             this.btnIniciar.Name = "btnIniciar";
             this.btnIniciar.Size = new System.Drawing.Size(64, 25);
             this.btnIniciar.TabIndex = 1;
-            this.btnIniciar.Text = "button1";
+            this.btnIniciar.Text = "Iniciar";
             this.btnIniciar.UseVisualStyleBackColor = true;
             this.btnIniciar.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -81,16 +98,6 @@
             this.comboBox1.TabIndex = 2;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(340, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(263, 40);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Maquina de Turing";
-            // 
             // tbEntrada
             // 
             this.tbEntrada.Location = new System.Drawing.Point(43, 154);
@@ -100,7 +107,8 @@
             // 
             // btnCargar
             // 
-            this.btnCargar.Location = new System.Drawing.Point(272, 154);
+            this.btnCargar.Enabled = false;
+            this.btnCargar.Location = new System.Drawing.Point(271, 154);
             this.btnCargar.Name = "btnCargar";
             this.btnCargar.Size = new System.Drawing.Size(75, 23);
             this.btnCargar.TabIndex = 5;
@@ -115,20 +123,20 @@
             // 
             // btnDetener
             // 
-            this.btnDetener.Location = new System.Drawing.Point(200, 180);
+            this.btnDetener.Location = new System.Drawing.Point(154, 4);
             this.btnDetener.Name = "btnDetener";
             this.btnDetener.Size = new System.Drawing.Size(75, 23);
             this.btnDetener.TabIndex = 6;
-            this.btnDetener.Text = "button1";
+            this.btnDetener.Text = "Parar";
             this.btnDetener.UseVisualStyleBackColor = true;
             this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
             // 
             // tbVelocidad
             // 
-            this.tbVelocidad.LargeChange = 100;
-            this.tbVelocidad.Location = new System.Drawing.Point(43, 211);
+            this.tbVelocidad.LargeChange = 10;
+            this.tbVelocidad.Location = new System.Drawing.Point(3, 34);
             this.tbVelocidad.Maximum = 1000;
-            this.tbVelocidad.Minimum = 100;
+            this.tbVelocidad.Minimum = 10;
             this.tbVelocidad.Name = "tbVelocidad";
             this.tbVelocidad.Size = new System.Drawing.Size(194, 45);
             this.tbVelocidad.TabIndex = 7;
@@ -138,39 +146,115 @@
             // dgvTranciciones
             // 
             this.dgvTranciciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Green;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvTranciciones.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.Green;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvTranciciones.DefaultCellStyle = dataGridViewCellStyle8;
+            this.dgvTranciciones.Enabled = false;
             this.dgvTranciciones.Location = new System.Drawing.Point(466, 138);
             this.dgvTranciciones.Name = "dgvTranciciones";
             this.dgvTranciciones.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             this.dgvTranciciones.Size = new System.Drawing.Size(510, 157);
             this.dgvTranciciones.TabIndex = 8;
             // 
+            // btnnext
+            // 
+            this.btnnext.Location = new System.Drawing.Point(73, 5);
+            this.btnnext.Name = "btnnext";
+            this.btnnext.Size = new System.Drawing.Size(75, 23);
+            this.btnnext.TabIndex = 9;
+            this.btnnext.Text = "Paso";
+            this.btnnext.UseVisualStyleBackColor = true;
+            this.btnnext.Click += new System.EventHandler(this.btnnext_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 40);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Pasos :";
+            // 
+            // lbpasos
+            // 
+            this.lbpasos.AutoSize = true;
+            this.lbpasos.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbpasos.Location = new System.Drawing.Point(163, 269);
+            this.lbpasos.Name = "lbpasos";
+            this.lbpasos.Size = new System.Drawing.Size(35, 40);
+            this.lbpasos.TabIndex = 12;
+            this.lbpasos.Text = "0";
+            // 
+            // lbEstado
+            // 
+            this.lbEstado.AutoSize = true;
+            this.lbEstado.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEstado.Location = new System.Drawing.Point(166, 309);
+            this.lbEstado.Name = "lbEstado";
+            this.lbEstado.Size = new System.Drawing.Size(0, 40);
+            this.lbEstado.TabIndex = 11;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnIniciar);
+            this.panel1.Controls.Add(this.btnDetener);
+            this.panel1.Controls.Add(this.tbVelocidad);
+            this.panel1.Controls.Add(this.btnnext);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(52, 180);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(276, 86);
+            this.panel1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Estado: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(345, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 40);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Maquina de Turing";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1004, 371);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbpasos);
+            this.Controls.Add(this.lbEstado);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.dgvTranciciones);
-            this.Controls.Add(this.tbVelocidad);
-            this.Controls.Add(this.btnDetener);
             this.Controls.Add(this.btnCargar);
             this.Controls.Add(this.tbEntrada);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.btnIniciar);
             this.Controls.Add(this.DgvCinta);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.DgvCinta)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tbVelocidad)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvTranciciones)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,13 +265,19 @@
         private System.Windows.Forms.DataGridView DgvCinta;
         private System.Windows.Forms.Button btnIniciar;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tbEntrada;
         private System.Windows.Forms.Button btnCargar;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button btnDetener;
         private System.Windows.Forms.TrackBar tbVelocidad;
         private System.Windows.Forms.DataGridView dgvTranciciones;
+        private System.Windows.Forms.Button btnnext;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbpasos;
+        private System.Windows.Forms.Label lbEstado;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Proyecto MT/Proyecto MT/Form1.cs
+++ Proyecto MT/Proyecto MT/Form1.cs
@@ -21,6 +21,7 @@ namespace Proyecto_MT
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            
             //DgvCinta.ColumnCount++;
             //DgvCinta.Columns[DgvCinta.ColumnCount-1].Width = 50;
             //DgvCinta.CurrentCell = DgvCinta[15, 0];
@@ -40,10 +41,11 @@ namespace Proyecto_MT
             {
                 
                 DgvCinta.Columns[i].Width = 50;
+                DgvCinta.Rows[0].Cells[i].Value = "";
             }
             DgvCinta.Rows[0].Height = 50;
-            DgvCinta.CurrentCell = DgvCinta[0, 0];
-            DgvCinta.CurrentCell.Style.SelectionBackColor = Color.Green;
+            DgvCinta.CurrentCell = DgvCinta[1, 0];
+   
             DgvCinta.Font = new Font("Tahoma", 15);
 
         }
@@ -72,15 +74,18 @@ namespace Proyecto_MT
                    dgvTranciciones.Rows[j].Cells[i].Value = maquina.tabla[j, i] ;
                 }
             }
-            //dgvTranciciones.CurrentCell = DgvCinta[0, 0];
-            //dgvTranciciones.CurrentCell.Style.SelectionBackColor = Color.Green;
             dgvTranciciones.Font = new Font("Tahoma", 7);
 
         }
 
         private void btnCargar_Click(object sender, EventArgs e)
         {
+            pasos = 0;
+            timer1.Enabled = false;
+            panel1.Enabled = true;
             InitializeGrid();
+            inicializarmaquina();
+            lbEstado.ForeColor = System.Drawing.Color.Black;
             int tamañoCinta = DgvCinta.ColumnCount;
             string entrada = tbEntrada.Text;
             if(tamañoCinta<= entrada.Length)
@@ -89,20 +94,22 @@ namespace Proyecto_MT
                 {
                     DgvCinta.ColumnCount++;
                     DgvCinta.Columns[DgvCinta.ColumnCount - 1].Width = 50;
+                    DgvCinta.Rows[0].Cells[DgvCinta.ColumnCount - 1].Value = "";
                 }
             }
             for (int i = 0; i < entrada.Length; i++)
             {
                 DgvCinta.Rows[0].Cells[i+1].Value = entrada.Substring(i,1);
             }
+            lbEstado.Text = maquina.estadoActal;
+            lbpasos.Text= pasos.ToString();
 
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            pasos++;
-            DgvCinta.CurrentCell = DgvCinta[pasos, 0];
-            DgvCinta.CurrentCell.Style.SelectionBackColor = Color.Green;
+            
+            leerCinta(DgvCinta.CurrentCell.Value.ToString());
 
         }
 
@@ -115,29 +122,111 @@ namespace Proyecto_MT
         {
             timer1.Interval = tbVelocidad.Value;
         }
+        private void leerCinta(string valorcinta)
+        {
+            int columnat=0;
+            int filat=0;
+            string trancision;
+            bool simbolocorrecto = false;
+            for (int i = 0; i < maquina.simbolosCinta.Length; i++)
+            {
+                string[] columnaParse = maquina.simbolosCinta[i].Split(',');
+                if(columnaParse[0]== valorcinta)
+                {
+                    columnat = Convert.ToInt32(columnaParse[1]);
+                    simbolocorrecto = true;
+                }
+            }
+            if(simbolocorrecto)
+            {
+                string[] filaParse = maquina.estadoActal.Split('q');
+                filat = Convert.ToInt32(filaParse[1]);
+                trancision = maquina.tabla[filat, columnat];
+                dgvTranciciones.CurrentCell = dgvTranciciones[columnat, filat];
+                if (trancision == "")
+                {
+                    lbEstado.Text = "Error cadena rechazada";
+                    lbEstado.ForeColor = System.Drawing.Color.Red;
+                    timer1.Enabled = false;
+                    panel1.Enabled = false;
+                }
+                else
+                {
+                    string[] funcion = trancision.Split(',');
+                    maquina.estadoActal = funcion[0];
+                    DgvCinta.CurrentCell.Value = funcion[1];
+                    if (funcion[2] == "R")
+                    {
+                        DgvCinta.CurrentCell = DgvCinta[DgvCinta.CurrentCell.ColumnIndex + 1, 0];
+                    }
+                    else
+                    {
+                        DgvCinta.CurrentCell = DgvCinta[DgvCinta.CurrentCell.ColumnIndex - 1, 0];
+                    }
+                    pasos++;
+                    lbEstado.Text = maquina.estadoActal;
+                    lbpasos.Text = pasos.ToString();
+                    for (int i = 0; i < maquina.estadosAceptacion.Length; i++)
+                    {
+                        if (maquina.estadosAceptacion[i] == maquina.estadoActal)
+                        {
+                            lbEstado.Text = maquina.estadoActal + " es un estado de aceptacion";
+                            lbEstado.ForeColor = System.Drawing.Color.YellowGreen;
+                            timer1.Enabled = false;
+                            panel1.Enabled = false;
+                        }
+                    }
+                }
+                
 
+            }
+            else
+            {
+                lbEstado.Text = "Simbolo no aceptado";
+                timer1.Enabled = false;
+            }
+            
+
+
+
+        }
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnCargar.Enabled = true;
+            panel1.Enabled = false;
+            inicializarmaquina();
+
+        }
+
+        private void btnnext_Click(object sender, EventArgs e)
+        {
+            leerCinta(DgvCinta.CurrentCell.Value.ToString());
+        }
+        public void inicializarmaquina()
         {
             int numeromaquina = comboBox1.SelectedIndex;
             if (numeromaquina == 0)
             {
-
+                maquina = new Maquina(new string[,] { { "q1,,R", "q2,,R","q3,,R", "q4,,R" },
+                                                    { "q1,a,R", "q1,b,R","q1,c,R", "q5,,L" },
+                                                    { "q2,a,R", "q2,b,R","q2,c,R", "q6,,L" },
+                                                    { "q3,a,R", "q3,b,R","q3,c,R", "q7,,L" },
+                                                    { "", "","", "" },
+                                                    { "q8,,L", "","", "q4,,R" },
+                                                    { "", "q8,,L","", "q4,,R" },
+                                                    { "", "","q8,,L", "q4,,R"},
+                                                    { "q9,,L", "q10,,L","q11,,L", "q4,,R" },
+                                                    { "q9,a,L", "q9,b,L","q9,c,L", "q12,,R" },
+                                                    { "q10,a,L", "q10,b,L","q10,c,L", "q13,,R" },
+                                                    { "q11,a,L", "q11,b,L","q11,c,L", "q14,,R" },
+                                                    { "q0,,R", "","", "q4,,R" },
+                                                    { "", "q0,,R","", "q4,,R" },
+                                                    { "", "","q0,,R", "q4,,R"}},
+                                                   new string[] { "a", "b", "c" }, new string[] { "a,0", "b,1", "c,2", ",3" }, "q0", new string[] { "q4" });
             }
             else if (numeromaquina == 1)
             {
-                maquina = new Maquina(new string[,] { { "q1,\"\",R", "q2,\"\",R","q3,\"\",R", "q4,\"\",R" },
-                                                    { "q1,a,R", "q1,b,R","q1,c,R", "q5,\"\",L" },
-                                                    { "q2,a,R", "q2,b,R","q2,c,R", "q6,\"\",L" },
-                                                    { "q3,a,R", "q3,b,R","q1,c,R", "q7,\"\",L" },
-                                                    { "", "","", "" },
-                                                    { "q8,\"\",L", "","", "q4,\"\",R" },
-                                                    { "", "q8,\"\",L","", "q4,\"\",R" },
-                                                    { "", "","q8,\"\",L", "q4,\"\",R"},
-                                                    { "q9,\"\",L", "q10,\"\",L","q11,\"\",L", "q4,\"\",R" },
-                                                    { "q9,a,L", "q9,b,L","q9,c,L", "q0,\"\",R" },
-                                                    { "q10,a,L", "q10,b,L","q0,c,L", "q0,\"\",R" },
-                                                    { "q11,a,L", "q11,b,L","11,c,L", "q0,\"\",R" }},
-                                                    new string[] { "a", "b", "c" }, new string[] { "a,0", "b,1", "c,2", ",4" }, "q0", new string[] { "q4" });
+               
                 InitializeGridtraciones();
             }
             else if (numeromaquina == 2)
@@ -148,7 +237,6 @@ namespace Proyecto_MT
             {
 
             }
-
         }
     }
 }
